8d0d2cb91fd0e935fab59810133ac598071c9372
Pramod Singh Manyal <manyalp12345@gmail.com>
2023-12-22 07:38:42 +0545

    OOP

--- Ecommerce.py
+++ Ecommerce.py
@@ -1,80 +1,177 @@
-# (Intermmediate) Task : Ecommerce application with two users buyer and seller seller can add product and
-# view there products buyer can view all products from different sellers and can purchase one product,
-# after purchase a bill listing total amount for buyer should be printed out
+# (Intermmediate) Task : Ecommerce application
+# Requirements
+# - Ask user login or register
+# - If regiser ask if he/she is buyer or seller
+# - While login if user is buyer, give him choices : 1. View ,2. Buy 3. View bills
+# - If user is seller, give him choices, give him/her : 1. Add products, View his/her products, 3. View his/her billed products.
 
-# Global variables to store product information
-products = []
+import json
 
+# Register User Function
+def register():
+    username = input('Enter your username : ')
+    password = input('Enter your password : ')
+    user_type = input('Enter your user type :Buyer or Seller ').lower()
 
+    user_data = {'username':username,'password':password, 'user_type':user_type}
+    json_user_data = json.dumps(user_data)
+    f = open('user_db.txt','a')
+    f.write(json_user_data + '-')
+    f.close()
+    print('Account created successfully!')
 
-# Add product
-def add_product():
-    product_name = input("Enter product name: ")
-    price = float(input("Enter product price: "))
-    seller_name = input("Enter seller name: ")
+# User login Function
+def login():
+    username = input('Enter your username : ')
+    password = input('Enter your password : ')
+    f = open('user_db.txt','r')
+    content = f.read()
+    f.close()
+    list_data = content.split('-')
+    login = False
+    user_user_type = None
+    for i in list_data:
+        if i != '':
+            user_dict_data = json.loads(i)
+            if username == user_dict_data.get('username') and password == user_dict_data.get('password'):
+                login = True
+                user_user_type = user_dict_data.get('user_type')
 
-# define product dictionary
-    product = {
-        "name": product_name,
-        "price": price,
-        "seller": seller_name
-    }
-
-    products.append(product)
-    print("Product added successfully!")
-# View Products seller wise
-def view_products(seller_name):
-    print("\nYour Products:")
-    for product in products:
-        if product["seller"] == seller_name:
-            print(f"Product: {product['name']}, Price: Rs.{product['price']}")
-
-# View All Products from products dictionary
-def view_all_products():
-    print("\nAll Products:")
-    for product in products:
-        print(f"Product: {product['name']}, Price: Rs. {product['price']}, Seller: {product['seller']}")
-
-    #buyer to purchase a product
-    purchase = input("Do you wanna purchase a product? (y/n): ")
-    if purchase.lower() == "y":
-
-        product_index = int(input("Enter the index of the product you want to purchase: "))
-
-        if 0 <= product_index < len(products):
-            selected_product = products[product_index]
-
-            total_amount = product['price']
-
-            print("\nBill:")
-            for product in products:
-                print(f"Product: {product['name']}, Price: Rs.{product['price']}")
-
-            print(f"Total Amount: Rs.{total_amount}")
+    if login ==True:
+        print('Login successful.')
+        if user_user_type == 'buyer':
+            print('''
+                    1. View all products
+                    2. View all your bills
+                  ''')
+            user_choice = input('Enter a choice : ')
+            if user_choice == '1':
+                view_products(username)
+            elif user_choice == '2':
+                view_bills(username)
         else:
-            print("Invalid product index.")
+            print('''
+                    1. Add product
+                    2. View all products
+                    3. View all bills
+                  ''')
+            user_choice = input('Enter a choice : ')
+            if user_choice == '1':
+                add_product(username)
+            elif user_choice == '2':
+                view_seller_products(username)
+            elif user_choice == '3':
+                view_bills(username)
+
     else:
-        print("Returning to main menu.")
+        print('Invalid Login.')
+
+# Buyer View Products Function
+def view_products(buyer):
+    f = open('products.txt','r')
+
+    content = f.read()
+
+    list_product_data = content.split('-')
+
+    for i in list_product_data:
+        print(i)
+
+    user_purchase_product_name = input('Enter the product name you want to purchase : ').lower()
+    user_purchase_product_quantity = int(input('Enter the quantity you want to purchase : '))
+
+    for i in list_product_data:
+        if i != '':
+            product_dict_data = json.loads(i)
+            if product_dict_data.get('product_name') == user_purchase_product_name:
+                product_data = product_dict_data
+                product_price = product_dict_data.get('product_price')
+                int_product_price = int(product_price)
+
+    total = user_purchase_product_quantity * int_product_price
+
+    print(f'Your total is : {total}')
+
+    purchase_product_dict_data = {"product_name":product_data.get('name'),"product_price":product_data.get('price'),"quantity":user_purchase_product_quantity,"total":total,"seller":product_data.get('seller'),"buyer":buyer}
+    json_product_data = json.dumps(purchase_product_dict_data)
+
+    f = open('bill.txt','a')
+    f.write(json_product_data + '-')
+    f.close()
+
+#Buyer View Bills function
+def view_bills(username):
+    f = open('bill.txt','r')
+    content = f.read()
+    f.close()
+
+    list_bill_data = content.split('-')
+
+    for i in list_bill_data:
+        if i != '':
+            bill_dict_data = json.loads(i)
+            if bill_dict_data.get('buyer') == username:
+                print(i)
+
+# Add Product by seller
+def add_product(seller):
+    product_name = input('Enter Product name : ')
+    product_price = input('Price of Product : ')
+    product_description = input('Enter Product Description ')
+
+    product_data = {'product_name':product_name, 'product_price':product_price, 'product_description':product_description, 'seller':seller}
+    json_user_data = json.dumps(product_data)
+    f = open('products.txt','a')
+    f.write(json_user_data + '-')
+    f.close()
+    print('Product Added successfully!')
+
+# Seller View Products Function
+def view_seller_products(seller):
+    f = open('products.txt', 'r')
+    content = f.read()
+    f.close()
+
+    list_product_data = content.split('-')
+
+    for i in list_product_data:
+        if i != '':
+            product_dict_data = json.loads(i)
+            if product_dict_data.get('seller') == seller:
+                print(i)
+
+#Seller View Bills Function
+def view_seller_bills(seller):
+    f = open('bill.txt', 'r')
+    content = f.read()
+    f.close()
+
+    list_bill_data = content.split('-')
+
+    for i in list_bill_data:
+        if i != '':
+            bill_dict_data = json.loads(i)
+            if bill_dict_data.get('seller') == seller:
+                print(i)
 
 
-# Main Ecommerce Program
 def main():
     while True:
-        print("1. Seller: Add Product")
-        print("2. Seller: View Products")
-        print("3. Buyer: View All Products")
-        print("4. Exit")
+        print("1. Create Account")
+        print("2. Login")
+        print("3. Exit")
 
-        choice = int(input("Enter your choice: "))
-
-        if choice == 1:
-            add_product()
-        elif choice == 2:
-            seller_name = input("Enter your name as a seller: ")
-            view_products(seller_name)
-        elif choice == 3:
-            view_all_products()
-        elif choice == 4:
+        user_in = input("Enter your choice: ")
+        try:
+            user_input = int(user_in)
+        except ValueError:
+            print("Invalid input. Please enter a number.")
+            continue
+        if user_input == 1:
+            register()
+        elif user_input == 2:
+            login()
+        elif user_input == 3:
             print("Exiting the program.")
             break
         else:
@@ -82,4 +179,3 @@ def main():
 # main constructor
 if __name__ == "__main__":
     main()
-
